02004c1e6942d71b851b41afac6aea9c311b776e
Kaarel Kalmus <kaakalmu@tktk.ee>
2021-11-19 18:06:38 +0200

    JS-sandbox

---
+++ js/randomImage.js
@@ -0,0 +1,16 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector ('img');
+const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpeg", "pic4.jpeg"];
+
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage () {
+
+    console.log(image);
+
+    let randomImage = Math.floor(Math.random() * images.length);
+    console.log(randomImage);
+    image.src = "images/pic1.jpeg";
+    image.style.backroundImage = images[randomImage];
+}
